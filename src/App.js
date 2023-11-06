7c53017aa157656f0831f7a874318187baa42542
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-06 23:27:27 +0900

    ビルド失敗の修正対応

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PokemonThumbnails from './PokemonThumbnails';
 import Header from './Header';
 
