2636bd502e7e9fe7cbfdd459ef354e0041eea492
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-06 16:41:15 +0900

    format

--- src/App.js
+++ src/App.js
@@ -1,66 +1,69 @@
-import { useEffect, useState } from 'react';
-import PokemonThumbnails from './PokemonThumbnails';
+import { useEffect, useState } from "react";
+import PokemonThumbnails from "./PokemonThumbnails";
 
 function App() {
   const [allPokemons, setAllPokemons] = useState([]);
   const pokemons = [
     {
       id: 1,
-      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      type: "くさ"
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      type: "くさ",
     },
     {
       id: 2,
-      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-      type: "くさ"
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+      type: "くさ",
     },
     {
       id: 3,
-      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
-      type: "くさ"
+      image:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+      type: "くさ",
     },
-  ]
+  ];
   // APIからデータを取得する
   // パラメータにlimitを設定し、20件取得する
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
   // 仮でフシギダネのURLを使用する
-  const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/bulbasaur"
+  const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
 
   const getAllPokemons = () => {
     fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data.results)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
         setAllPokemons(data.results);
 
         // 次の20件をURLにセットする
         setUrl(data.next);
-      })
-  }
+      });
+  };
 
   const createPokemonObject = (results) => {
-    results.forEach(pokemon => {
-      const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+    results.forEach((pokemon) => {
+      const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
       fetch(pokemonUrl)
-        .then(res => res.json())
-        .then(data => {
+        .then((res) => res.json())
+        .then((data) => {
           const image = data.sprites.other["official-artwork"].front_default;
           const type = data.types[0].type.name;
           console.log(data.name, image, type);
-        })
-    })
-  }
+        });
+    });
+  };
 
   useEffect(() => {
     getAllPokemons();
     createPokemonObject();
-  }, [])
+  }, []);
 
   return (
     <div className="app-container">
       <h1>ポケモン図鑑</h1>
-      <div className='pokemon-container'>
-        <div className='all-container'>
+      <div className="pokemon-container">
+        <div className="all-container">
           {pokemons.map((pokemon, index) => (
             <PokemonThumbnails
               id={pokemon.id}
