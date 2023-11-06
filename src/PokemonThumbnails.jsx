6b25fb6dc1fc9f29184b3837bbccda43b8c13bea
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-06 17:55:49 +0900

    表示を日本語に

--- src/PokemonThumbnails.jsx
+++ src/PokemonThumbnails.jsx
@@ -1,15 +1,43 @@
-const PokemonThumbnails = ({ id, name, image, type }) => {
+import React, { useState, useEffect } from "react";
+import pokemonJson from "./pokemon.json";
+import pokemonTypeJson from "./pokemonType.json";
+
+const PokemonThumbnails = ({ id, name, image, iconImage, type }) => {
+  const style = `thumb-container ${type}`;
+
+  const [jpName, setJpName] = useState("");
+  const [jpType, setJpType] = useState("");
+
+  const setJapaneseName = async () => {
+    const target = await pokemonJson.find(
+      (pokemon) => pokemon.en.toLowerCase() === name
+    );
+    target ? setJpName(target.ja) : setJpName("???");
+  };
+
+  const setJapaneseType = async () => {
+    const target = await pokemonTypeJson[type];
+    target ? setJpType(target) : setJpType(type);
+  };
+
+  useEffect(() => {
+    setJapaneseName();
+    setJapaneseType();
+  }, []);
+
   return (
-    <div className="thumb-container grass">
+    <div className={style}>
       <div className="number">
         <small>#0{id}</small>
       </div>
       <img src={image} alt={name} />
+      <img src={iconImage} alt={name} className="icon-image" />
       <div className="detail-wrapper">
-        <h4>{name}</h4>
-        <h3>{type}</h3>
+        <h4>{jpName}</h4>
+        <h3>{jpType}</h3>
       </div>
     </div>
   );
 };
+
 export default PokemonThumbnails;
