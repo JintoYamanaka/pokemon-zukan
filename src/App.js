ebc6690c73bdf4ce87ef4411f7133f662fdfc380
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-08 01:07:17 +0900

    ローディングのバグ対応

--- src/App.js
+++ src/App.js
@@ -58,7 +58,8 @@ function App() {
 
   useEffect(() => {
     getAllPokemons();
-  }, [getAllPokemons]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="app-container">
